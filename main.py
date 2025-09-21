e31e172e858c6a9545585fa99b090babea2fd740
Jacob Alzapiedi <jalzapiedi3@gatech.com>
2025-09-21 17:13:27 -0400

    It **is** a Nash equilibrium

--- main.py
+++ main.py
@@ -22,9 +22,12 @@ def plot(config:Config, results:np.ndarray, title_prepend="") -> None:
     plt.show()
 if __name__=='__main__':
     # Make config
-    n = 10
+    n = 3
     num_player_samples = 30
     cfg = Config(n=n, pt_m=np.linspace(0,1,num_player_samples), pt_nm=np.linspace(0,1,num_player_samples))
+    delta = 0.001
+    # nash_val = 2/(n+1)
+    # cfg = Config(n=n, pt_m=np.linspace(nash_val-delta, nash_val+delta, num_player_samples), pt_nm=np.linspace(nash_val-delta, nash_val+delta, num_player_samples))
 
     # Monte Carlo
     mc_result = method1(cfg)
@@ -35,7 +38,17 @@ if __name__=='__main__':
 
     plot(cfg, results,"Analytic ")
 
+    p_n = 2/(cfg.n-1)
 
 
+
+
+
+    # TODO compare MC version to analytic version
+    # TODO get optimal pt_nm from given pt_m, and vice versa
+    # TODO Look at evolution of pts? When proportion of population is the same as the probability of win
+    # TODO Animation: Sliding through n, the population progression of evolutionary
+    # TODO Nash?
+    # TODO Make something that generates a number for you, given n
     pass
 
